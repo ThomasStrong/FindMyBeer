26cd7de73d518272232f9af6cd025cc3e9b907e0
Tom Strong <strng_thms@yahoo.com>
2022-07-11 14:24:07 -0400

    Small edits to index files, pseudocode

---
+++ index.js
@@ -0,0 +1,15 @@
+// getelementbyid username
+// use that for fetch() url
+// error? handle w promise and prompt for valid username if necessary
+
+// parse json of user's wishlist? (display 1-5?)
+// if wishlist is empty, fetch the day's highest rated beer and use that for calcs?
+// otherwise, display 1-5 wishlist items
+// if only one item, generate the list of venues
+
+// user picks item, distance range, then list of venues is generated via:
+// calculate using user's location and venues via lat-long
+
+// if beer is found nearby, then show the available locations (5 closest?)
+// Haversine formula?
+// populate venue name, address, phone, perhaps Google mapping
